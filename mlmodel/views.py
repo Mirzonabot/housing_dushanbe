b239e5f05559b7a005ec7c0261a542ce4eb2bc51
Mirzonabot <mirzonabot.mirzonabotov99@gmail.com>
2023-01-09 03:20:24 +0600

    all changes made

--- mlmodel/views.py
+++ mlmodel/views.py
@@ -1,25 +1,41 @@
 from django.shortcuts import render,HttpResponse,HttpResponseRedirect
 
-from .forms import NameForm, MyGeoForm
+from .forms import MyGeoForm
 
+import xgboost
+import pickle
+import numpy as np
+import json
+
+import utm
+from django.contrib.gis.geos import Point
+
+filename = "xgboost_model.sav"
+model = pickle.load(open(filename, 'rb'))
 
 # Create your views here.
 def index(request):
     # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        print("In if")
-        print(request.POST)
-        # create a form instance and populate it with data from the request:
-        # form = NameForm(request.POST)
-        # check whether it's valid:
-        # if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            # return HttpResponseRedirect('/thanks/')
 
-    # if a GET (or any other method) we'll create a blank form
-    
+    prediction = None
+
+    if request.method == 'POST':
+        response = request.POST
+        floor = response["floor"]
+        rooms = response["rooms"]
+        area = response["area_m_sqrd"]
+        location = response["point"]
+        location = json.loads(location)
+        lc = location["coordinates"]
+        point = Point(lc[0], lc[1], srid=3857)
+        point.transform(4326)
+        long = float(point.x)
+        lat  = float(point.y)
+        parameters = np.array([[rooms,floor,area,lat,long]], dtype=object)
+        prediction = model.predict(parameters)[0] 
+        print(prediction)
+        
+       
     form = MyGeoForm()
 
-    return render(request, 'index.html', {'form': form})
+    return render(request, 'index.html', {'form': form, "price":prediction})
