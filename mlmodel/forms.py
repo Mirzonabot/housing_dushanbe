4a6cadf39c455972a4313f8808f8cf0d91e8c5f8
Mirzonabot <mirzonabot.mirzonabotov99@gmail.com>
2023-01-08 23:34:18 +0600

    Style was added and form was recreated

--- mlmodel/forms.py
+++ mlmodel/forms.py
@@ -7,6 +7,14 @@ class NameForm(forms.Form):
 
 
 class MyGeoForm(gforms.Form):
+    floor = forms.IntegerField(max_value=20)
+    rooms = forms.IntegerField(max_value=20)
+    area_m_sqrd = forms.FloatField(max_value=300)
     point = gforms.PointField(widget=
-        gforms.OSMWidget(attrs={'map_width': 800, 'map_height': 500}))
-    # submit = forms.
+        gforms.OSMWidget(attrs={'map_width': 400, 
+                                'map_height': 500, 
+                                'default_lat':38.559772,
+                                'default_lon':68.787038,
+                                'default_zoom': 12}))
+
+
--- mlmodel/views.py
+++ mlmodel/views.py
@@ -7,17 +7,19 @@ from .forms import NameForm, MyGeoForm
 def index(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
+        print("In if")
+        print(request.POST)
         # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
+        # form = NameForm(request.POST)
         # check whether it's valid:
-        if form.is_valid():
+        # if form.is_valid():
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+            # return HttpResponseRedirect('/thanks/')
 
     # if a GET (or any other method) we'll create a blank form
-    else:
-        form = MyGeoForm()
+    
+    form = MyGeoForm()
 
     return render(request, 'index.html', {'form': form})
